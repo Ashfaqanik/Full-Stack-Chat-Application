6e26fb9938e4c8a530b6f1029729c1bc7146064a
Ashfaque <ashfaqanik698@gmail.com>
2025-01-19 15:42:45 +1100

    Homepage updated

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -5,11 +5,11 @@ import MessageContainer from "./MessageContainer";
 function HomePage() {
   return (
     <div
-      className="flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden
-     bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0"
+      className="flex flex-col h-auto md:h-[550px] rounded-lg overflow-hidden p-4
+   bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0"
     >
-      <Sidebar className="w-full sm:w-[30%] bg-blue-200" />
-      <MessageContainer className="w-full sm:w-[70%] bg-white" />
+      <Sidebar className="w-full bg-blue-200" />
+      <MessageContainer className="w-full bg-white" />
     </div>
     // <div
     //   className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden
