96ebbb27b4743cbc3a2181902ff9bebfce0b0e9a
Ashfaque <ashfaqanik698@gmail.com>
2025-01-19 16:49:58 +1100

    TItle updated

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -8,8 +8,8 @@ function HomePage() {
       className="flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden
      bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0"
     >
-      <Sidebar className="w-full sm:w-[30%] bg-blue-200" />
-      <MessageContainer className="w-full sm:w-[70%] bg-white" />
+      <Sidebar />
+      <MessageContainer />
     </div>
     // <div
     //   className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden
